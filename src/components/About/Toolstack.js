a732431ca822f0bb292b03bedd3d01168855486e
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2023-07-02 14:16:28 +0530

    Revert "updated portfolio website"
    This reverts commit a6648c2f5fde298ac8aa7d579e76b924c527fa11.

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import React from "react";
+import { Col, Row } from "react-bootstrap";
 import {
   SiLinux,
   SiVisualstudiocode,
   SiHeroku,
+  SiNetlify,
   SiGithub,
-} from 'react-icons/si';
+} from "react-icons/si";
 
 function Toolstack() {
   return (
-    <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
-      <Col xs={4} md={2} className='tech-icons'>
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
         <SiLinux />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className="tech-icons">
         <SiGithub />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNetlify />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <SiHeroku />
       </Col>
     </Row>
