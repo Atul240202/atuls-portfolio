67580949d15a65dd5e1046a1eb97033e30f668d2
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2023-07-02 14:16:45 +0530

    Revert "Made some changes in Home page"
    This reverts commit 9ba9dcdcc93431fcff1559a5f0d7dfa8fe2f5e91.

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,82 +1,86 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import myImg from '../../Assets/Atul.jpg';
-import Tilt from 'react-parallax-tilt';
-import { AiFillGithub, AiFillInstagram } from 'react-icons/ai';
-import { FaLinkedinIn } from 'react-icons/fa';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/Atul.jpg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
   return (
-    <Container fluid className='home-about-section' id='about'>
+    <Container fluid className="home-about-section" id="about">
       <Container>
         <Row>
-          <Col md={8} className='home-about-description'>
-            <h1 style={{ fontSize: '2.6em' }}>
-              LET ME <span className='purple'> INTRODUCE </span> MYSELF
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
-            <p className='home-about-body'>
-              I am an web developer, with passion to learn new skills &
-              implement them in real time to solve some problem. Because thats
-              what a Engineer do 🤷‍♂️
+            <p className="home-about-body">
+              I am an Enthusiast, with passion to learn new skills & implement them in real time to solve some problem. 
+              Because thats what a Engineer do 🤷‍♂️
               <br />
-              <br />I have started publishing my own content on{' '}
-              <a href='https://medium.com/@atuljha2402/about'>Medium</a> based
-              on
+              <br />I am fluent in classics like
               <i>
-                <b className='purple'> Javascript </b>
+                <b className="purple"> C++ & Javascript </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className='purple'>Web Technologies </b> as its give me an
-                opportunity to showcase my creativity
+                <b className="purple">Web Technologies </b> as its give me an opportunity to showcase my creativity
               </i>
-              I am also a Patent holder with &nbsp;
+              <br />
+              Whenever possible, I also apply my passion for developing products
+              with <b className="purple">React.js, Next.js & Node.js</b>         
+              <br />
+              I am also a Patent holder with  &nbsp;
+
               <i>
-                <b className='purple'> 7+ </b> Filed patents
-              </i>
-            </p>
+                <b className="purple"> 7+ </b> Filed patents
+              </i>    
+              </p>
           </Col>
-          <Col md={4} className='myAvtar'>
+          <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className='img-fluid' alt='avatar' />
+              <img src={myImg} className="img-fluid" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
         <Row>
-          <Col md={12} className='home-about-social'>
+          <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
             <p>
-              Feel free to <span className='purple'>connect </span>with me
+              Feel free to <span className="purple">connect </span>with me
             </p>
-            <ul className='home-about-social-links'>
-              <li className='social-icons'>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
                 <a
-                  href='https://github.com/atul240202'
-                  target='_blank'
-                  rel='noreferrer'
-                  className='icon-colour  home-social-icons'
+                  href="https://github.com/atul240202"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
                 >
                   <AiFillGithub />
                 </a>
               </li>
-              <li className='social-icons'>
+              <li className="social-icons">
                 <a
-                  href='https://www.linkedin.com/in/atuljha24/'
-                  target='_blank'
-                  rel='noreferrer'
-                  className='icon-colour  home-social-icons'
+                  href="https://www.linkedin.com/in/atuljha24/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
                 >
                   <FaLinkedinIn />
                 </a>
               </li>
-              <li className='social-icons'>
+              <li className="social-icons">
                 <a
-                  href='https://www.instagram.com/atuljha.24/'
-                  target='_blank'
-                  rel='noreferrer'
-                  className='icon-colour home-social-icons'
+                  href="https://www.instagram.com/atuljha.24/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
                 >
                   <AiFillInstagram />
                 </a>
