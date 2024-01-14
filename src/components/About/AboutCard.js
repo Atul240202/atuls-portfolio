0bf005ee003503283ec5468e191931890f8ea736
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2024-01-15 02:11:10 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className='blockquote mb-0'>
           <p style={{ textAlign: 'justify' }}>
             Hello Everyone, I am <span className='purple'>Atul Jha </span>
-            from <span className='purple'> Varanasi, India.</span>
+            from <span className='purple'> Noida, India.</span>
             <br /> I have completed my Bachelor's in Computer Science &
             Engineering from Chandigarh University, Mohali
             <br />
@@ -18,27 +18,27 @@ function AboutCard() {
           </p>
           <ul>
             <li className='about-activity'>
-              <ImPointRight /> Former Software Engineer Intern at Capgemini
+              <ImPointRight /> Software Engineer at Capgemini (Nov'23 - present)
             </li>
             <li className='about-activity'>
-              <ImPointRight /> Former Technical Intern at Newton School
+              <ImPointRight /> Former Technical Intern at Newton School (Aug'22 - Aug'23)
             </li>
             <li className='about-activity'>
-              <ImPointRight /> Patent Holder with 7 filed patents
+              <ImPointRight /> Patent Holder with 5 filed patents
             </li>
             <li className='about-activity'>
-              <ImPointRight /> Former Chairperson of IEEE CTSoc CUSB
+              <ImPointRight /> Former Chairperson of IEEE Consumer technology Chandigarh University Student Branch [CTSoc CUSB] (Oct'21 - Oct'22)
             </li>
             <li className='about-activity'>
               <ImPointRight /> Former Industrial Relations Team Lead at IEEE
-              India Council
+              India Council (Jan'22 - Jan'23)
             </li>
             <li className='about-activity'>
               <ImPointRight /> Vice President of Innovtor's Hub (College
               community of Patents & Project)
             </li>
             <li className='about-activity'>
-              <ImPointRight /> Delivered 20+ Sessions & Hosted 40+ Technical
+              <ImPointRight /> Delivered 20+ Sessions & Hosted 50+ Technical
               events.
             </li>
           </ul>
